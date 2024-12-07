b66de4f96e594ea6304fa563990fa5442b778374
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-12-08 00:28:00 +0100

    Update access-control-lab-02.py

--- Broken-Access-Control/access-control-lab-02.py
+++ Broken-Access-Control/access-control-lab-02.py
@@ -1,7 +1,7 @@
 import requests 
 import sys
 import urllib3 
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup  
 import re   
  
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
