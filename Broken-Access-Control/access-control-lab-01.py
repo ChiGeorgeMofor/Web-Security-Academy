2fd6dd54587b8bbf95c1f6d3a86e83768bdf6215
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-12-08 00:30:26 +0100

    Update access-control-lab-01.py

--- Broken-Access-Control/access-control-lab-01.py
+++ Broken-Access-Control/access-control-lab-01.py
@@ -5,7 +5,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
   
 proxies = {'http': 'https://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
  
-def delete_user(url):
+def delete_user(url): 
     admin_panel_url = url + '/administrator-panel'
     r = requests.get(admin_panel_url, verify=False, proxies=proxies)
     if r.status_code == 200:
